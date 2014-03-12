91d9daaf363ab7e462b8139f1e66ada3dd7202d4
kevcenteno <kevin@oldeyorke.ca>
2014-03-12 16:24:06 -0400

    created an init for calculation storage; got it to run only when getFinalCost is called.  made the applyMarkup functions take in the specific variables instead of an object

--- src/Calculator.js
+++ src/Calculator.js
@@ -3,58 +3,53 @@
   "use strict";
 
   function Calculator() {
-    this.baseMarkup = 0.05;
+    this.flatMarkup = 0.05;
     this.personMarkup = 0.012;
     this.materialMarkup = {
       "drugs" : 0.075,
       "food" : 0.13,
       "electronics" : 0.02
     };
-
-    this._subTotal = 0.00;
-    this._peopleMarkup = 0.00;
-    this._materialMarkup = 0.00;
  }
 
   Calculator.prototype.getFinalCost = function (job) {
     var finalCost, parsedJob;
 
-    this._subTotal = 0.00;
-    this._peopleMarkup = 0.00;
-    this._materialMarkup = 0.00;
+    this._initCalculationStorage();
 
     parsedJob = this._parseJob(job);
 
-    this._applyFlatMarkup(parsedJob);
-    this._applyPeopleMarkup(parsedJob);
-    this._applyMaterialMarkup(parsedJob);
+    this._applyFlatMarkup(parsedJob.basePrice);
+    this._applyPeopleMarkup(parsedJob.people);
+    this._applyMaterialMarkup(parsedJob.material);
 
     finalCost = this._subTotal + this._peopleMarkup + this._materialMarkup;
+
     return '$' + this._roundToNearestPenny(finalCost);
   };
 
-  Calculator.prototype._applyFlatMarkup = function (parsedJob) {
+  Calculator.prototype._applyFlatMarkup = function (basePrice) {
     var markedUpPrice;
 
-    markedUpPrice = parsedJob.basePrice * (1 + this.baseMarkup);
+    markedUpPrice = basePrice * (1 + this.flatMarkup);
 
     this._subTotal = markedUpPrice;
   };
 
-  Calculator.prototype._applyPeopleMarkup = function (parsedJob) {
+  Calculator.prototype._applyPeopleMarkup = function (people) {
     var markupPercentageTotal;
 
-    markupPercentageTotal = this.personMarkup * parsedJob.people;
+    markupPercentageTotal = this.personMarkup * people;
 
     this._peopleMarkup = this._subTotal * markupPercentageTotal;
   };
 
-  Calculator.prototype._applyMaterialMarkup = function (parsedJob) {
+  Calculator.prototype._applyMaterialMarkup = function (material) {
     var markupPercentage;
 
-    if (!(parsedJob.material in this.materialMarkup)) return;
+    if (!(material in this.materialMarkup)) return;
 
-    markupPercentage = this.materialMarkup[parsedJob.material];
+    markupPercentage = this.materialMarkup[material];
 
     this._materialMarkup = this._subTotal * markupPercentage;
   };
@@ -73,6 +68,12 @@
     };
 
     return parsedJob;
+  };
+
+  Calculator.prototype._initCalculationStorage = function () {
+    this._subTotal = 0.00;
+    this._peopleMarkup = 0.00;
+    this._materialMarkup = 0.00;
   };
 
   window.Calculator = Calculator;
